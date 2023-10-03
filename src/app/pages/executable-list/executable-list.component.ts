b3ab3794cbfea8bae9cccde4e6124872dfd6bdc0
kabellrics <yflechel@gmail.com>
2023-10-03 23:10:47 +0200

    Start Select Plateforme for Executable

--- src/app/pages/executable-list/executable-list.component.ts
+++ src/app/pages/executable-list/executable-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExecutableService } from '../../services/executableService';
+import { Executable } from '../../model/executable';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-executable-list',
@@ -8,12 +10,31 @@ import { ExecutableService } from '../../services/executableService';
 })
 export class ExecutableListComponent implements OnInit {
   constructor(private executableService: ExecutableService) { }
-  ngOnInit(): void {
-    this.executableService.getExecutableData().subscribe(data => {
-      this.filteredexecutables = this.executables = data;
-    });
-  }
   displayedColumns: string[] = ['id', 'nom', 'type', 'favoris', 'actif', 'link'];
   executables: any;
   filteredexecutables: any;
+  plateformefilter: string[] = [];
+  selectedplateforme = new FormControl(this.plateformefilter[0]);
+  get filteredItems() {
+    if (this.selectedplateforme.value == "-1")
+      return this.executables;
+    else
+      return this.executables.filter((x: Executable) => !this.selectedplateforme.value || x.PlateformeId === this.selectedplateforme.value);
+  }
+
+  changefilteredData() {
+    if (this.selectedplateforme.value == "-1")
+      this.filteredexecutables = this.executables;
+    else
+      this.filteredexecutables = this.executables.filter((x: Executable) => !this.selectedplateforme.value || x.PlateformeId === this.selectedplateforme.value);
+  }
+
+  ngOnInit(): void {
+    this.executableService.getExecutableData().subscribe(data => {
+      this.executables = data;
+      this.filteredexecutables = this.executables;
+      const uniquePlateformeIds: string[] = Array.from(new Set(this.executables.map((x: Executable) => x.PlateformeId.toString())));
+      this.plateformefilter = ["-1", ...uniquePlateformeIds];      
+    });
+  }
 }
--- src/app/shared/plateformePipe.ts
+++ src/app/shared/plateformePipe.ts
@@ -1,13 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { PlateformeService } from '../services/plateformeService';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 @Pipe({ name: 'plateformename' })
 export class PlateformePipe implements PipeTransform {
   constructor(private plateformeService: PlateformeService) { }
   transform(value: string): Observable<string> {
-    return this.plateformeService.getPlateformeData().pipe(
-      map(data => data.find(x => x.Id === value)?.Name || '')
-    );
+    if (value != "-1") {
+      return this.plateformeService.getPlateformeData().pipe(
+        map(data => data.find(x => x.Id === value)?.Name || '')
+      );
+    }
+    else
+    {
+      return of("Toutes les plateformes");
+    }
   }
 }
